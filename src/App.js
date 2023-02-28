09b969c0b80e3062a826b85ecdff2921eff7fb20
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-02-28 01:36:33 -0800

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Input, LinearProgress, Typography,Stack, Container, Card, CardHeader, TextField, ImageList, ImageListItem, Grid, CardActionArea, CardMedia, CardActions } from '@mui/material';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import { deleteObject, getDownloadURL, listAll, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from './firebase/Firebase';
+import { uuidv4 } from '@firebase/util';
+import UpLoadimage from './component/UpLoadimage';
+import LoginPage from './component/LoginPage';
 
 function App() {
+
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [imageList, setImageList] = useState([]);
+  const imagelistRef = ref(storage, 'img/')
+
+
+ 
+
+  const deleteHandler = (ref, url) => {
+    deleteObject(ref).then((res) =>{
+      setImageList(imageList.filter((img) => img.url !== url))
+      alert('seccessfully deleted')
+    })
+
+  }
+
+ 
+
+  const uploadfile = () => {
+    // console.log('up load image', image.name)
+    if(image == null) return;
+
+    const imageRef = ref(storage, `img/${image.name + uuidv4()}`)
+    const upLoadFile = uploadBytesResumable(imageRef, image)
+
+    upLoadFile.on("state_changed", (snapshot) =>{
+      const progress = Math.round(snapshot.bytesTransferred/snapshot.
+      totalBytes * 100);
+      setProgress(progress)
+    },(err) =>{
+
+    }, () => {
+      setProgress(0);
+      getDownloadURL(upLoadFile.snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, {url: url, ref: uploadfile.snapshot.ref}])
+      })
+      alert('uploaded seccessfully ');
+
+    }
+    ) ;
+    setImage(null)
+
+  }
+  // get dataaaa
+
+  useEffect(() => {
+    
+      const getData = () => {
+        listAll(imagelistRef).then((res) => {
+          res.items.map((item) =>{
+            getDownloadURL(item).then((url) =>{
+              setImageList((prev) => [...prev, {url: url, ref: item}])
+            })
+          })
+
+        })
+      } 
+    getData();
+  }, [])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <LoginPage/> */}
+      <UpLoadimage/>
     </div>
   );
 }
